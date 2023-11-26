e905c8fa3f86ee1afdec0d2c1f4cdabed3d113c5
tyler kim <yoonsoo.kim11@gmail.com>
2023-11-26 11:22:26 -0500

    completed up to deliverable 1 and started 2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 console.log('Write your code here');
+
+fetch('http://127.0.0.1:3000/current-exhibits')
+.then(resp => resp.json())
+.then(exhibits => {
+    exhibits.forEach(exhibit => {
+
+    const title = document.getElementById('exhibit-title')
+    const description = document.getElementById('exhibit-description')
+    const image = document.getElementById('exhibit-image')
+    const tickets = document.getElementById('tickets-bought')
+    const comments = document.getElementById('comments-section')
+    const commentPElement = document.createElement('p')
+
+    title.textContent = exhibit.title
+    description.textContent = exhibit.description
+    image.src = exhibit.image
+    tickets.textContent = exhibit.tickets_bought
+    
+    commentPElement.textContent = exhibit.comments
+    comments.appendChild(commentPElement)
+
+
+})
+
+})
